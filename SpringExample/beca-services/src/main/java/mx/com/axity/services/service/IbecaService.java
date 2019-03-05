38ff2e7661fc5b91610feb58abfae1d80068e062
Francisco Casales <jose.casales@outlook.com>
2019-03-04 20:12:05 -0600

    Primer acceso a base de datos desde spring

--- SpringExample/beca-services/src/main/java/mx/com/axity/services/service/IbecaService.java
+++ SpringExample/beca-services/src/main/java/mx/com/axity/services/service/IbecaService.java
@@ -1,13 +1,15 @@
 package mx.com.axity.services.service;
 
 import mx.com.axity.commons.to.UserTO;
+import mx.com.axity.model.UserDO;
+
 import java.util.List;
 
 public interface IbecaService {
 
-    List<UserTO> getUsers();
     int sum(int sum1, int sum2);
     int subtraction(int subs1, int subs2);
     int multiplication(int mult1, int mult2);
     int division(int div1, int div2);
+    List<UserDO> getAllUsers();
 }
